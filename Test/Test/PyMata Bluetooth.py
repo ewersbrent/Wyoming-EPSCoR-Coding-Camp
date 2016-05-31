02f93be77c0874f9238da9860232be1d8874e4a9
milkman24 <speedymaco@gmail.com>
2016-05-31 15:34:52 -0600

    Pymata

---
+++ Test/Test/PyMata Bluetooth.py
@@ -0,0 +1,92 @@
+# PyMata Bluetooth
+# Author Matt Cook
+# Created May 31, 2016
+# Written for Wyoming EPSCoR Summer Coding Camp
+# Version 0.1
+
+# Attempting Bluetooth Communication with SoftwareSerial Library
+
+
+
+# import necessary libraries
+import serial
+import pyfirmata
+from time import sleep
+
+# Setup how we view the Arduino.  This is necessary because there
+# are multiple styles of Arduinos out there.  We need to make sure
+# we are using the right board (Arduino = Arduino Uno).
+board = pyfirmata.Arduino()
+
+# Open serial port to communicate with the Arduino
+# We shouldn't need to use Software Serial because we are
+# powering the Arduino with a battery, but we may have to.
+# NOTE: CHANGE THE PORT NUMBER IF YOU NEED TO
+connection = serial.Serial('/dev/ttyAMA0', 9600)
+
+# Wait for the two machines to synchronize
+sleep(3)
+
+
+# Define a function to make the user input into something
+# the Arduino can understand.
+def instructArduino(instruction):
+    # Make sure the input can be read by the Arduino
+    toArduino = int(instruction)
+    # Double check if connection is available
+    if connection.isOpen():
+        # Sends the instruction to the Arduino over our connection
+        connection.write(toArduino)
+    else:
+        # We use double quotes (") here so we can use an apostrophe (') in "isn't"
+        print("The Arduino isn't connected...")
+
+
+# Define a function to make the input from the Arduino into
+# something we want to see.
+def readArduino():
+    # Read from the Arduino until we are ready
+    # NOTE: the second number is how much data is waiting for us sent from the Arduino.
+    # This is an example of how we can use buffers to save data for us until we want it
+    return connection.read(connection, connection.in_waiting())
+    # TO DO: Make the output human readable/useful
+
+
+# Start a loop to ask the user how they want the Arduino to act.
+# Option 1: We ask the Arduino for a reading when we want.
+while True:
+
+
+    # This is called a "Try Block".  This structure allows us to stop the
+    # program without unplugging everything.
+    try:
+        if connection.is_open():
+            instruction = input("What do you want from the Arduino?")
+            instructArduino(instruction)
+            # Prints the results of our readArduino function
+            print (readArduino())
+
+    except:
+        # The user pressed the keys to quit (ctrl+C)
+        KeyboardInterrupt()
+        # Stop our connections so we don't waste resources
+        board.exit()
+        connection.close()
+
+# Option 2: We keep communication with the Arduino open.  The Arduino
+# will report what it observes at set intervals (to save battery and
+# prevent buffer overflow).
+while True:
+    # This is called a "Try Block".  This structure allows us to stop the
+    # program without unplugging everything.
+    try:
+        if connection.is_open():
+            # Prints the results of our readArduino function
+            print (readArduino())
+
+    except:
+        # The user pressed the keys to quit (ctrl+C)
+        KeyboardInterrupt()
+        # Stop our connections so we don't waste resources
+        board.exit()
+        connection.close()
--- Test/Test/Test.py
+++ Test/Test/Test.py
@@ -2,57 +2,69 @@
 # Author Matt Cook
 # Created May 23, 2016
 # Written for Wyoming EPSCoR Summer Coding Camp
+# Version 0.2
 
-# Version 0.1
+# EDITS:
+# Added support for Software Serial
+# Removed pyfirmata from use. Arduino provides buffer overflow protection.
 
-from Arduino import Arduino
-import pyfirmata
-from time import sleep
+import Arduino
+# import pyfirmata
+# from time import sleep
 
-board = pyfirmata.Arduino('COM3')
-sleep(3)
+board = Arduino.arduino.Arduino(9600,'COM3',2,None)
+
+# Leave in case we
+#board = pyfirmata.Arduino('COM3')
+#sleep(3)
 
 # Using iterator thread to avoid buffer overflow
 # If you want to know what this does, follow this link:
 # http://pyfirmata.readthedocs.io/en/latest/pyfirmata.html
-it = pyfirmata.util.Iterator(board)
-it.start()
+# it = pyfirmata.util.Iterator(board)
+# it.start()
 
-# define a describing variable so we don't have to type
-# a huge amount every time we use this library
-ser = Arduino.SoftwareSerial
+# We named a variable so it is easier for others to understand
+# what is going on.  This is called using a "describing variable".
+bluetoothSerial = Arduino.arduino.SoftwareSerial(board)
 
-# Setup virtual serial port using pin 10 to transmit (tx)
+# Setup "virtual" serial port using pin 10 to transmit (tx)
 # and pin 11 to recieve (rx).  The rate of data transmission
-# or baud rate is 9600 bits per second
-ser.begin(10, 11, 9600)
+# or baud rate is 9600 bits per second.
+bluetoothSerial.begin(10,11,9600)
 
 # Set pin 13 to make the onboard LED blink
-pinLED = board.get_pin("d:13:o")
+# pinLED = board.get_pin("d:13:o")
+pinLED = board.pinMode(13,"OUTPUT")
 
 # send test message to the other device
-ser.write("Hello from Arduino!")
+bluetoothSerial.write("Hello from Arduino!")
 
 # define a function to change LED state
-def changeLED(switchLED):
-    if(switchLED == 1 or switchLED == 0):
-        # Turn LED on or off (0=off 1=on)
-        board.digital[pinLED].write(switchLED)
+def changeState(state):
+
+    if state == 1:
+        # Turn LED ON (HIGH = ON)
+         board.digitalWrite(pinLED,"HIGH")
+
+    elif state == 0:
+        # Turn LED OFF (LOW = OFF)
+        board.digitalWrite(pinLED,"LOW")
+
     else:
         # if input is wrong, send error to original sender
-        ser.write("wrong input") # TODO: make this print a correct error message
-    pass
+        bluetoothSerial.write("wrong input") # TODO: make this print a correct error message
 
-# Test if a device is connected to our bluetooth. 
-if(ser.connected()):
+# Test if a device is connected to our bluetooth.
+# if bluetoothSerial.connected:
 
-    while(ser.connected()):
+    while True: # bluetoothSerial.connected:
         # Read first character sent from the other device, store it in 'a'
-        a = ser.read()
+        a = bluetoothSerial.read()
 
         # Send the character to our function to handle
-        changeLED(a)
+        changeState(a)
 
 
 
-board.exit()
+board.close()
